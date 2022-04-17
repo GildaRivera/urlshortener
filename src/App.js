c3210d203de7436be13c53c9024c2630eb307509
gildarivera <gilda.rivera@telus.com>
2022-04-17 14:50:44 -0600

    feat: adding final changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState();
+  const [shortUrl, setShortUrl] = useState("");
+  const shortenUrl = useRef();
+  const longUrl=useRef()
+  const [clean, setClean]= useState(true)
+  const createUrl = async () => {
+    if (url) {
+      const rawResponse = await fetch(`http://localhost:8080/api/hash`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: url }),
+      });
+      let response = await rawResponse.json();
+      setShortUrl(response.url);
+    }
+  };
+  useEffect(() => {
+    shortenUrl.current.value = shortUrl;
+  }, [shortUrl]);
+  const handleChange = (e) => {
+    if(e.target.value!==""){
+      setClean(false)
+    }
+    else{
+      setClean(true)
+    }
+    setUrl(e.target.value);
+  };
+  const handleClean = ()=>{
+shortenUrl.current.value=""
+longUrl.current.value=""
+  }
   return (
     <div>
-      <div className="container" >
+      <div className="container" style={{"maxWidth":"70%"}}>
         <div
           className="notification is-primary"
           style={{
             backgroundColor: "#e6820966",
             position: "relative",
-            top: "5vh",
+            top: "15vh",
           }}
         >
-          <h1 className="title is-1 has-text-centered" style={{marginBottom:"10vh"}}>URL Shortener </h1>
+          <h1
+            className="title is-1 has-text-centered"
+            style={{ marginBottom: "10vh" }}
+          >
+            URL Shortener
+          </h1>
           <div style={{ display: "flex", justifyContent: "center" }}>
             <input
               className="input is-warning"
               type="text"
+              onChange={handleChange}
               placeholder="Your URL"
-              style={{ width: "90%", borderRadius:"15px" }}
+              style={{ width: "90%", borderRadius: "15px" }}
+              ref={longUrl}
             />
+      {clean? <></>:      <button className="clear" onClick={handleClean}><i className="fa-solid fa-x"></i></button>}
+
           </div>
           <div
             style={{
@@ -36,16 +81,23 @@ function App() {
                 borderColor: "transparent",
                 color: "rgba(0,0,0,.7)",
               }}
+              onClick={createUrl}
             >
               Shorten url
             </button>
+            
           </div>
           <div style={{ display: "flex", justifyContent: "center" }}>
             <input
               className="input is-warning"
               type="text"
               placeholder="Shorten URL"
-              style={{ width: "90%",borderRadius:"15px", marginBottom:"5vh" }}
+              style={{
+                width: "90%",
+                borderRadius: "15px",
+                marginBottom: "5vh",
+              }}
+              ref={shortenUrl}
             />
           </div>
         </div>
